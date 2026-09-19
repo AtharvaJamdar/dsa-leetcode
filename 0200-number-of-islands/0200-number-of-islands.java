7fb6cfe305fc1ecad0403d6ce9c623dd88ab0c3b
AtharvaJamdar <atharvajamdar1@gmail.com>
2026-09-19 07:52:55 +0530

    Time: 3 ms (87.2%), Space: 52.5 MB (21.43%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,34 @@
+class Solution {
+    int m;
+    int n;
+    public int numIslands(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        int island = 0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == '1'){
+                    island++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return island;
+    }
+
+    public void dfs(char[][] grid, int r,int c){
+
+        if(r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == '0'){
+            return;
+        }
+
+        grid[r][c] = '0';
+
+        dfs(grid, r-1, c);
+        dfs(grid, r+1, c);
+        dfs(grid, r, c-1);
+        dfs(grid, r, c+1);
+    }
+
+}
